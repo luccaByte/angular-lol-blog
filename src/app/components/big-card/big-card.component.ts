ef92162a3e8a1d583e552dd6982853d01491336f
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-16 15:47:21 +0000

    transferindo conteúdo do big card para o typescript

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class BigCardComponent {
 
+  @Input()
+  photoCover: string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardDescription: string = ""
 }
