2eeb0c6bb472d0bd885e89a4812284b0ec6e8d35
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-16 17:35:01 +0000

    adicionando id para os components

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "#",
+        "title": "",
+        "description": ""
+    },
+    {
+        "id": 2,
+        "photo": "#",
+        "title": "",
+        "description": ""
+    }
+]
